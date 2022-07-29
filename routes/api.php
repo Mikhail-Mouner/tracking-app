cad60d3df78620789e4011662aea1de908146b0e
Mikhail-Mouner <mikhaeelmouner@yahoo.com>
2022-07-29 16:11:45 +0200

    Routes Api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\{AuthenticationController, BabyController, PartnerController};
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +13,31 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+##############* Route:: Register & Login user *##############
+Route::post('/create-account', [AuthenticationController::class, 'createAccount']);
+Route::post('/signin', [AuthenticationController::class, 'signin']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+##############* Middleware (auth) *##############
+Route::middleware('auth:sanctum')->group(function () {
+
+    ##############* Route:: CRUD (Baby) *##############
+    Route::apiResource('/baby', BabyController::class);
+
+    ##############* Route:: Create & index (Partner) *##############
+    Route::apiResource('/partner', PartnerController::class)->only('index', 'store');
+
+    ##############* Route:: His Babies *##############
+    Route::get('/parent/babies', [BabyController::class, 'myBaby']);
+
+    ##############* Route:: Profile & Sign Out *##############
+    Route::get('/me', [AuthenticationController::class, 'me']);
+    Route::post('/sign-out', [AuthenticationController::class, 'signout']);
+
 });
+
+##############* Route:: Not Found Page *##############
+Route::any('{path}', function () {
+    return response()->json([
+        'message' => 'Route not found'
+    ], 404);
+})->where('path', '.*');
