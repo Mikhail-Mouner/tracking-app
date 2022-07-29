07f840dc0f82b338576a009db97479253b88599c
Mikhail-Mouner <mikhaeelmouner@yahoo.com>
2022-07-29 16:11:33 +0200

    Add Info To Swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,19 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *    title="Tracking App API",
+ *    version="1.0.0",
+ * )
+ * @OA\SecurityScheme(
+ *       scheme="Bearer",
+ *       securityScheme="Bearer",
+ *       type="apiKey",
+ *       in="header",
+ *       name="Authorization",
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
